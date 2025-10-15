582f99fb0f90855fa5aa913356019d37b2ab6bfc
هيثم <43355014+Al-Maghribi@users.noreply.github.com>
2025-10-15 13:22:40 -0400

    sql+php v1

--- form1.php
+++ form1.php
@@ -2,6 +2,22 @@
 <html>
     <head>
         <title>Form response</title>
+        <?php
+            $server = "localhost";
+            $username = "php";
+            $password = "ben!2020";
+            $database = "userinfo";
+            $conn = mysqli_connect($server, $username, $password, $database);
+            
+            // Check for successful connection
+            if (!$conn) {
+              die("Connection failed: {mysqli_connect_error()}");
+            }
+            $sql = "INSERT INTO info (fname, lname, age) VALUES ('Hermione', 'Granger', 16);";
+
+            $result = mysqli_query($conn, $sql);
+        ?>
+
     </head>
 
     <body>
