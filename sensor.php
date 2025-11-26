1df03460576c9b6066b15d3a155635e0348abf5a
هيثم <43355014+Al-Maghribi@users.noreply.github.com>
2025-11-26 00:02:13 -0500

    Update sensor.php

--- sensor.php
+++ sensor.php
@@ -14,16 +14,6 @@
     <body>
         <p> <?php echo "Temperature: $temp °C"; ?> </p>
         <p> <?php echo "Pressure: $pressure hPa"; ?> </p>
-        <p> <?php echo "Altitude: $altitude m"; ?> </p>
-
-
-        <p><<?php
-echo "User: " . shell_exec("whoami") . "<br>";
-$raw = shell_exec("pwd");
-echo "PWD: $raw<br>";
-$raw = shell_exec("./bme280 2>&1"); // capture errors
-var_dump($raw);
-?>/p>
-        
+        <p> <?php echo "Altitude: $altitude m"; ?> </p>        
     </body>
 </html>
